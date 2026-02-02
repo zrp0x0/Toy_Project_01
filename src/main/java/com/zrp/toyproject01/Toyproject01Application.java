d061ab65a3845132f5b4c747fcefdd487cf528dd
zrp0x0 <zrp0x0@gmail.com>
2026-02-02 21:10:23 +0900

    first commit

---
+++ src/main/java/com/zrp/toyproject01/Toyproject01Application.java
@@ -0,0 +1,13 @@
+package com.zrp.toyproject01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Toyproject01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Toyproject01Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/zrp/toyproject01/Toyproject01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zrp.toyproject01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Toyproject01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
